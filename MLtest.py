6acd1967098e08ab543db5d4794b7b27334a15e2
Thepingwinman <115560704+Thepingwinman@users.noreply.github.com>
2023-11-21 09:22:07 +0100

    Add files via upload

--- MLtest.py
+++ MLtest.py
@@ -7,10 +7,38 @@ def Get_textlist():
     #print(len(webpage_words))
     amount_of_words_in_dok = len(webpage_words)
     definitiv_list = []
-    for i in range(amount_of_words_in_dok):
-        if webpage_words[i] not in definitiv_list:
-            definitiv_list.append(webpage_words)
-    print(len(definitiv_list))
+    test_list = ["hello", "yeti", "hello", "pablo", "escobar", "is", "my", "name"]
+    test_list_words = len(test_list)
+    for i in range(test_list_words):
+        if test_list[i] not in definitiv_list:
+            definitiv_list.append(test_list[i])
+        """value = binsok(definitiv_list, test_list[i])
+        if value == False:
+            definitiv_list.append(test_list[i])"""
+    print(definitiv_list)
+
+def binsok(lista, element):
+    '''Kollar om ett element finns i en lista mha binärsökning.
+
+    Parametrar:
+    lista (list): listan som undersöks
+    elem (str): vi vill se om detta element finns i listan.
+
+    Returvärden: ett booleskt värde beroende på om elementet finns i listan eller ej.'''
+    lista.sort()
+    
+    lo = 0
+    hi = len(lista)-1
+    while lo <= hi:
+        mid = (lo+hi)//2
+        #print(f'\n{lista[mid]}\n')
+        if element < lista[mid]:
+            hi = mid - 1
+        elif element > lista[mid]:
+            lo = mid + 1
+        else:
+            return True
+    return False
 
 if __name__ == "__main__":
     Get_textlist()
