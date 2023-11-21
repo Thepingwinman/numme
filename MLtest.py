026f806f36326e2ed048daaabb7cab70061e5e3a
Thepingwinman <115560704+Thepingwinman@users.noreply.github.com>
2023-11-21 10:04:16 +0100

    Add files via upload

--- MLtest.py
+++ MLtest.py
@@ -1,21 +1,49 @@
 import requests
+import PyPDF2
 
-def Get_textlist():
+
+def get_textlist():
     webpage = requests.get('https://www.gutenberg.org/cache/epub/72142/pg72142.txt')
     webpage_words = webpage.text.split()
+    webpage_words.sort()
     #print(webpage_words)
     #print(len(webpage_words))
     amount_of_words_in_dok = len(webpage_words)
     definitiv_list = []
-    test_list = ["hello", "yeti", "hello", "pablo", "escobar", "is", "my", "name"]
+
+    test_list = ['a', 'a', 'a', 'a', '123453', 'g', 'g', 'asdfg', 'dfghjk', 't', 't', 't', 'o']
+    test_list.sort()
+
+    pdfFileObj = open('text_data.pdf', 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    pageObj = pdfReader.pages[34]
+    print(pageObj.extract_text())
+    pdfFileObj.close()
+
     test_list_words = len(test_list)
-    for i in range(test_list_words):
-        if test_list[i] not in definitiv_list:
-            definitiv_list.append(test_list[i])
-        """value = binsok(definitiv_list, test_list[i])
-        if value == False:
-            definitiv_list.append(test_list[i])"""
+
+    actual_word = ''
+
+    """for i in range(amount_of_words_in_dok):
+        if webpage_words[i] != actual_word:
+            definitiv_list.append(webpage_words[i])
+            actual_word = webpage_words[i]"""
+
+        
     print(definitiv_list)
+
+def read_pdf_list(pdf):
+    pdfFileObj = open(pdf, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    ant_sidor = len(pdfReader.pages)
+    pg_lista = []
+    for i in range(ant_sidor):
+        page_obj = pdfReader.pages[i]
+        text = page_obj.extract_text()
+        pg_lista.append(text.split())
+    text_lista = []
+    [text_lista.extend(el) for el in pg_lista]
+    print(text_lista)
 
 def binsok(lista, element):
     '''Kollar om ett element finns i en lista mha binärsökning.
@@ -41,4 +69,5 @@ def binsok(lista, element):
     return False
 
 if __name__ == "__main__":
-    Get_textlist()
+    #get_textlist()
+    read_pdf_list("text_data.pdf")
