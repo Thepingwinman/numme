3fe490e91a4ac9f1909a978f58b5120796a8570a
oLganesson118 <151193433+oLganesson118@users.noreply.github.com>
2023-12-08 13:20:20 +0100

    SKA ANVÄNDAS FÖR INLÄMNING; Update MLtest.py

--- MLtest.py
+++ MLtest.py
@@ -2,12 +2,12 @@ import PyPDF2
 
 def get_unique_word_list(word_list):
     '''
-    Given a list, this function returns a list with the same elements, but without repetition. (That is, elements that occur multiple times in word_list will only
-    occur one time in the unique_word_list that this function returns.)
+    Given a list, this function creates a list with the same elements, but without repetition and writes it to a file. (That is, elements that occur multiple times in word_list will only
+    occur one time in the unique_word_list that this function creates.)
 
     Parameters: word_list (list): list of words (strings) that may include repetitions.
 
-    Return values: unique_word_list (list): list of words (strings) that does not include repetitions.
+    Return values: none.
     '''
     word_list.sort()
 
@@ -19,7 +19,7 @@ def get_unique_word_list(word_list):
 
         word = word_list[i]
 
-        word.strip('.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower()
+        word.strip('$%&+=0123456789*.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower()
 
         #Create unique list
         if word != actual_word:
@@ -27,23 +27,22 @@ def get_unique_word_list(word_list):
             actual_word = word
 
     write2file(unique_word_list, 'unique_word_list.txt')
-    #return unique_word_list
 
 def get_word_list(pdf):
-    '''Reads pdf-file and returns list of all words that occure.
+    '''Reads pdf-file and creates a list of all words that occure, that is then returned and printed to a file.
     
     Parameters: pdf (str): the name of the pdf-file.
     
-    Return values: word_list (list): list of all words that occur in the pdf-file.'''
+    Return values: word_list (list): list of strings that contains all words in the infile.'''
 
     pdf_obj = open(pdf, 'rb')
-    pdfReader = PyPDF2.PdfReader(pdf_obj)
-    number_of_pages = len(pdfReader.pages)
+    pdf_reader = PyPDF2.PdfReader(pdf_obj)
+    number_of_pages = len(pdf_reader.pages)
     
     word_matrix = []
 
     for i in range(number_of_pages):
-        page_obj = pdfReader.pages[i]
+        page_obj = pdf_reader.pages[i]
         text = page_obj.extract_text()
         word_matrix.append(text.split())
     
@@ -53,15 +52,10 @@ def get_word_list(pdf):
     word_list_edited = []
 
     for i in range(len(word_list)):
-        if i in list(range(15)):
-            print(f'\nordet: {word_list[i]}\n')
-            
-            print(f'\nangtal karaktärer i ordet: {len(word_list[i])}\n\n')
-
-        if word_list[i].strip('.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower() in ['', ' ', '\n']:
+        if word_list[i].strip('$%&+=0123456789*.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower() in ['', ' ', '\n']:
             continue
-        
-        word_list_edited.append(word_list[i].strip('.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower())
+
+        word_list_edited.append(word_list[i].strip('$%&+=0123456789*.?!-)(][_/|,"“”:}{;—<>@').strip("'").lower())
 
     write2file(word_list_edited, 'word_list.txt')
     return word_list_edited
@@ -75,11 +69,10 @@ def write2file(word_list, file_name):
     
     Return values: none.'''
 
-    with open(file_name, 'w') as outfile:
+    with open(file_name, 'w', encoding='utf-8') as outfile:
         for word in word_list:
             outfile.write(word + '\n')
         
 if __name__ == '__main__':
-    word_list = get_word_list('text_data.pdf')
+    word_list = get_word_list('Andersen.pdf')
     unique_word_list = get_unique_word_list(word_list)
-
